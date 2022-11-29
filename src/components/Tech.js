06a1c83a72a24521aaa9647ca5b4555c9b5193c2
Abigail Kaye Unating <abbyunat@gmail.com>
2022-11-29 22:59:07 +0800

    Added tech

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -37,17 +37,23 @@ export default function Tech() {
             <motion.h1 variants={transition} className="font-bold text-2xl lg:text-4xl pb-10"> &lt;technologies/&gt;</motion.h1>
             <motion.div variants={container} ref={ref} initial="hidden" animate={animationControl} className="grid grid-cols-2 lg:grid-cols-3 text-md lg:text-2xl px-2 lg:p-0 gap-x-8 lg:gap-x-20 text-center">
                 <motion.div variants={transition}>html</motion.div>
-                <motion.div variants={transition}>tailwind.css</motion.div>
-                <motion.div variants={transition}>mysql</motion.div>
                 <motion.div variants={transition}>css</motion.div>
-                <motion.div variants={transition}>next.js</motion.div>
-                <motion.div variants={transition}>git & github</motion.div>
                 <motion.div variants={transition}>javascript</motion.div>
-                <motion.div variants={transition}>node.js</motion.div>
-                <motion.div variants={transition}>figma, adobe ai & ps</motion.div>
                 <motion.div variants={transition}>react</motion.div>
-                <motion.div variants={transition}>firebase</motion.div>
+                <motion.div variants={transition}>next.js</motion.div>
+                <motion.div variants={transition}>tailwind.css</motion.div>
+                <motion.div variants={transition}>angular.js</motion.div>
+                <motion.div variants={transition}>typescript</motion.div>
+                <motion.div variants={transition}>node.js</motion.div>
                 <motion.div variants={transition}>python</motion.div>
+                <motion.div variants={transition}>firebase</motion.div>
+                <motion.div variants={transition}>heroku</motion.div>
+                <motion.div variants={transition}>vercel</motion.div>
+                <motion.div variants={transition}>flutter</motion.div>
+                <motion.div variants={transition}>git & github</motion.div>
+                <motion.div variants={transition}>figma</motion.div>
+                <motion.div variants={transition}>adobe ai & ps</motion.div>
+                <motion.div variants={transition}>mysql</motion.div>
             </motion.div>
         </motion.div>
     )
