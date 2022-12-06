65d9acb1ce5971a2b3ae320aaed1d02ea080d2f5
Abigail Kaye Unating <abbyunat@gmail.com>
2022-12-06 22:31:35 +0800

    Fixed 24 inch intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -44,16 +44,16 @@ export default function Intro({setLoading}) {
     return (
         <motion.div onAnimationComplete={() => setLoading(false)} className='z-20 overflow-hidden absolute w-full h-[100vh]' initial="hidden" animate="show" exit="out" variants={circle}>
             <motion.div className='absolute bottom-[20rem] left-[-20rem] lg:bottom-[-5rem] md:left-[-20rem]' variants={circles}>
-                <span className='h-[1200px] w-[1200px] rounded-full bg-lightpink inline-block p-2'></span>
+                <span className='h-[1200px] w-[1200px] 2xl:h-[1400px] 2xl:w-[1400px] rounded-full bg-lightpink inline-block p-2'></span>
             </motion.div>
             <motion.div className='absolute right-[-18rem] bottom-[10rem] md:bottom-[1rem] md:right-[-8rem]' variants={circles}>
-                <span className='h-[620px] w-[620px] md:h-[900px] md:w-[900px] rounded-full bg-lightyellow inline-block p-2'></span>
+                <span className='h-[620px] w-[620px] md:h-[900px] md:w-[900px] 2xl:h-[1200px] 2xl:w-[1200px] rounded-full bg-lightyellow inline-block p-2'></span>
             </motion.div>
             <motion.div className='absolute right-[8rem] bottom-[0] md:bottom-[-20rem] md:left-[14rem]' variants={circles}>
-                <span className='h-[600px] w-[600px] md:h-[700px] md:w-[700px] rounded-full bg-lightred inline-block p-2'></span>
+                <span className='h-[600px] w-[600px] md:h-[700px] md:w-[700px] 2xl:h-[900px] 2xl:w-[900px] rounded-full bg-lightred inline-block p-2'></span>
             </motion.div>
             <motion.div className='absolute bottom-[-15rem] right-[-13rem] md:right-[-1rem] md:bottom-[-25rem] md:right-[-5rem]' variants={circles}>
-                <span className='h-[600px] w-[600px] md:h-[700px] md:w-[700px] rounded-full bg-lightgreen inline-block p-2'></span>
+                <span className='h-[600px] w-[600px] md:h-[700px] md:w-[700px] 2xl:h-[900px] 2xl:w-[900px] rounded-full bg-lightgreen inline-block p-2'></span>
             </motion.div>
         </motion.div>
     )
