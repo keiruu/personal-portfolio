cd3be1b236250cb593055e2c449751ed6d706106
Abigail Kaye Unating <abbyunat@gmail.com>
2023-01-02 22:38:53 +0800

    Added dynamic date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,13 @@ const Footer = ({ topRef }) => {
     topRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
+  const year = new Date().getFullYear();
+
   return (
     <div className='bg-lightgray w-full flex flex-col items-center justify-center py-10'>
       <h3 onClick={top} className='cursor-pointer font-bold text-lg mb-3 hover:opacity-60'>back to top</h3>
       <p className='text-xs lg:text-sm text-gray'>Made with ❤️, Next.js, React, & Tailwind.css</p>
-      <p className='text-xs lg:text-sm text-gray'>2022 © Abigail Kaye Unating • All Rights Reserved.</p>
+      <p className='text-xs lg:text-sm text-gray'>{year} © Abigail Kaye Unating • All Rights Reserved.</p>
     </div>
   );
 };
